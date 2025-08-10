9b2e4ea367125c4c1c445c103ff2bda730e4fbea
briboy359 <briboy359@gmail.com>
2025-08-10 14:40:10 +0300

    Update set.js

--- set.js
+++ set.js
@@ -20,18 +20,18 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 const config = {
     session: process.env.SESSION_ID || '', //paste your session here 
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Keith", //replace with your owner name
-    NUMERO_OWNER: process.env.NUMERO_OWNER || "254748387615",   //replace with your owner number  
+    OWNER_NAME: process.env.OWNER_NAME || "brayo", //replace with your owner name
+    NUMERO_OWNER: process.env.NUMERO_OWNER || "254717276195",   //replace with your owner number  
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',    
     URL: process.env.URL || "https://files.catbox.moe/4h8lfw.jpg",    //replace with your image url                     
     ANTICALL_MSG: process.env.ANTICALL_MSG || 'call declined',             
     GURL: process.env.GURL || "https://github.com/Keithkeizzah", // replace with your url
     EVENTS: process.env.EVENTS || "yes",    
-    BOT: process.env.BOT_NAME || 'ALPHA-MD', //replace with your bot name
-    MODE: process.env.PUBLIC_MODE || "yes",              
+    BOT: process.env.BOT_NAME || 'Boyer-MD', //replace with your bot name
+    MODE: process.env.PUBLIC_MODE || "no",              
     TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", //replace with your timezone 
     DP: process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM: process.env.ANTI_DELETE_MESSAGE || 'yes',
+    ADM: process.env.ANTI_DELETE_MESSAGE || 'no',
     
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
