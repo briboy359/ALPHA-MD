05a2cf1a4176f45d5866c8163ea955d435111703
briboy359 <briboy359@gmail.com>
2025-08-12 18:04:42 +0300

    Update autobio.js

--- database/autobio.js
+++ database/autobio.js
@@ -1,39 +1,3 @@
-const config = require("../set");
-const { DataTypes } = require('sequelize');
-
-const AutoBioDB = config.DATABASE.define('autobio', {
-    status: {
-        type: DataTypes.ENUM('on', 'off'),
-        defaultValue: 'on',
-        allowNull: false
-    },
-    message: {
-        type: DataTypes.STRING,
-        defaultValue: '🌟 Always active!',
-        allowNull: false
-    },
-    timezone: {
-        type: DataTypes.STRING,
-        defaultValue: 'Africa/Nairobi',
-        allowNull: false
-    }
-}, {
-    timestamps: false
-});
-
-async function initAutoBioDB() {
-    try {
-        await AutoBioDB.sync({ alter: true });
-        console.log('AutoBio table ready');
-    } catch (error) {
-        console.error('Error initializing AutoBio table:', error);
-        throw error;
-    }
-}
-
-async function getAutoBioSettings() {
-    try {
-        const [settings] = await AutoBioDB.findOrCreate({
             where: {},
             defaults: {}
         });
